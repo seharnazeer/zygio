e932483e83ed79acf9d607c54a0925a2a3104293
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-12-23 20:37:30 +0500

    new

--- routes/restaurantAuth.js
+++ routes/restaurantAuth.js
@@ -1,7 +1,7 @@
 // routes/restaurantAuth.js
 const express = require('express');
 const router = express.Router();
-const { registerRestaurant, loginRestaurant , updateRestaurantInfo, uploadProfilePicture } = require('../controller/restaurantAuth');
+const { registerRestaurant, loginRestaurant , updateRestaurantInfo, uploadProfilePicture } = require('../controller/restaurantAuth.js');
 const upload = require('../middleware/multer');
 
 /**
